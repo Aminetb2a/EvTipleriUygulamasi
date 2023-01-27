32a860d7fb4944e79efbbe2dfd10af17d874cae8
aminet2a <aminetbo@gmail.com>
2023-01-27 23:28:03 +0100

    #6 use stream in houses generation

--- src/patika/dev/definex/repos/Repository.java
+++ src/patika/dev/definex/repos/Repository.java
@@ -5,9 +5,10 @@ import patika.dev.definex.model.SummerHouse;
 import patika.dev.definex.model.Villa;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Repository {
     private final static int minRoom = 1;
@@ -19,56 +20,40 @@ public class Repository {
     private final static int minPrice = 10000;
     private final static int maxPrice = 9999999;
     private final static Random rand = new Random();
-
     public List<House> generateHouses(Integer houseNumber) {
-        List<House> houseList = new ArrayList<>();
-
-        for (Integer i = 1; i < houseNumber + 1; i++) {
-            houseList.add(
-                    House.builder()
-                            .id(i)
-                            .area(rand.nextDouble(maxArea - minArea + 1) + minArea)
-                            .roomNumber(rand.nextInt(maxRoom - minRoom + 1) + minRoom)
-                            .salonNumber(rand.nextInt(maxSalon - minSalon + 1) + minSalon)
-                            .price(BigDecimal.valueOf(rand.nextInt(maxPrice - minPrice + 1) + minPrice))
-                            .build()
-            );
-        }
-        return houseList;
+        return Stream.generate(() ->
+                House.builder()
+                        .id(rand.nextInt(houseNumber))
+                        .area(rand.nextDouble(maxArea - minArea + 1) + minArea)
+                        .roomNumber(rand.nextInt(maxRoom - minRoom + 1) + minRoom)
+                        .salonNumber(rand.nextInt(maxSalon - minSalon + 1) + minSalon)
+                        .price(BigDecimal.valueOf(rand.nextInt(maxPrice - minPrice + 1) + minPrice))
+                        .build()
+        ).limit(houseNumber).collect(Collectors.toList());
     }
 
     public List<Villa> generateVillas(Integer villaNumber) {
-        List<Villa> villaList = new ArrayList<>();
-
-        for (Integer i = 1; i < villaNumber + 1; i++) {
-            villaList.add(
-                    Villa.builder()
-                            .id(i)
-                            .area(rand.nextDouble(maxArea - minArea + 1) + minArea)
-                            .roomNumber(rand.nextInt(maxRoom - minRoom + 1) + minRoom)
-                            .salonNumber(rand.nextInt(maxSalon - minSalon + 1) + minSalon)
-                            .price(BigDecimal.valueOf(rand.nextInt(maxPrice - minPrice + 1) + minPrice))
-                            .build()
-            );
-        }
-        return villaList;
+        return Stream.generate(() ->
+                Villa.builder()
+                        .id(rand.nextInt(villaNumber))
+                        .area(rand.nextDouble(maxArea - minArea + 1) + minArea)
+                        .roomNumber(rand.nextInt(maxRoom - minRoom + 1) + minRoom)
+                        .salonNumber(rand.nextInt(maxSalon - minSalon + 1) + minSalon)
+                        .price(BigDecimal.valueOf(rand.nextInt(maxPrice - minPrice + 1) + minPrice))
+                        .build()
+        ).limit(villaNumber).collect(Collectors.toList());
     }
 
     public List<SummerHouse> generateSummerHouses(Integer summerHouseNumber) {
-        List<SummerHouse> houseList = new ArrayList<>();
-
-        for (Integer i = 1; i < summerHouseNumber + 1; i++) {
-            houseList.add(
-                    SummerHouse.builder()
-                            .id(i)
-                            .area(rand.nextDouble(maxArea - minArea + 1) + minArea)
-                            .roomNumber(rand.nextInt(maxRoom - minRoom + 1) + minRoom)
-                            .salonNumber(rand.nextInt(maxSalon - minSalon + 1) + minSalon)
-                            .price(BigDecimal.valueOf(rand.nextInt(maxPrice - minPrice + 1) + minPrice))
-                            .build()
-            );
-        }
-        return houseList;
+        return Stream.generate(() ->
+                SummerHouse.builder()
+                        .id(rand.nextInt(summerHouseNumber))
+                        .area(rand.nextDouble(maxArea - minArea + 1) + minArea)
+                        .roomNumber(rand.nextInt(maxRoom - minRoom + 1) + minRoom)
+                        .salonNumber(rand.nextInt(maxSalon - minSalon + 1) + minSalon)
+                        .price(BigDecimal.valueOf(rand.nextInt(maxPrice - minPrice + 1) + minPrice))
+                        .build()
+        ).limit(summerHouseNumber).collect(Collectors.toList());
     }
 
 
