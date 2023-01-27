79ff5b2041b7b804cc54199f6f27b9eafbf9e3ab
aminet2a <aminetbo@gmail.com>
2023-01-27 22:48:58 +0100

    #1 create the EvTipleriUygulamasiApplication

---
+++ src/patika/dev/definex/EvTipleriUygulamasiApplication.java
@@ -0,0 +1,15 @@
+package patika.dev.definex;
+
+
+import patika.dev.definex.service.BaseService;
+
+public class EvTipleriUygulamasiApplication {
+
+    public static void main(String[] args) {
+
+        BaseService service = new BaseService();
+
+    }
+
+}
+
